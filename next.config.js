4ad0849c0c12f466f61a59f54c190c2b667bebb3
Takuya Taniguchi <ttaniguchi@binc.jp>
2021-10-10 12:42:06 +0900

    Add vanilla-extract

--- next.config.js
+++ next.config.js
@@ -1,3 +1,26 @@
+const { VanillaExtractPlugin } = require("@vanilla-extract/webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const { merge } = require("webpack-merge")
+
 module.exports = {
+  webpack: (config) => {
+    return merge(config, {
+      module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, "css-loader"],
+          },
+        ],
+      },
+      plugins: [
+        new VanillaExtractPlugin(),
+        new MiniCssExtractPlugin({
+          filename: "static/chunks/[contenthash].css",
+          chunkFilename: "static/chunks/[contenthash].css",
+        }),
+      ],
+    })
+  },
   reactStrictMode: true,
 }
